3957b60099e17d873cc78c2c2ecb168f6321faea
MOHIT KUMAR <mohit18csu129@ncuindia.edu>
2019-01-15 11:24:18 +0530

    boxdemo6.java added

---
+++ boxdemo6.java
@@ -0,0 +1,22 @@
+class box{
+	double width,w;
+	double height,h;
+	double depth,d;
+	box(double width,double h,double d){
+	this.width=width;
+	this.height=h;
+	this.depth=d;
+	}
+	double volume(){
+	return width*height*depth;
+	}
+}
+class boxdemo6
+{
+	public static void main(String args[]){
+	double vol;
+	box box3=new box(12,34,67);
+	vol=box3.volume();
+	System.out.println(vol);
+	}
+}
